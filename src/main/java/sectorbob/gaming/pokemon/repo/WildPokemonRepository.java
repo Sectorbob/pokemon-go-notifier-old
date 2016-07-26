093481e84a3a253302dc11adbcde7dd9d3205311
Sectorbob <heidew@gmail.com>
2016-07-26 11:37:18 -0400

    Added a database to store the pokemon and notifications sent

--- src/main/java/sectorbob/gaming/pokemon/repo/WildPokemonRepository.java
+++ src/main/java/sectorbob/gaming/pokemon/repo/WildPokemonRepository.java
@@ -1,9 +1,8 @@
 package sectorbob.gaming.pokemon.repo;
 
-import org.springframework.stereotype.Component;
+import org.springframework.data.jpa.repository.JpaRepository;
 import sectorbob.gaming.pokemon.model.Pokemon;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -11,56 +10,8 @@ import java.util.List;
  *
  * Created by ltm688 on 7/24/16.
  */
-@Component
-public class WildPokemonRepository {
+public interface WildPokemonRepository extends JpaRepository<Pokemon, Long> {
 
-    private List<Pokemon> wildPokemon;
-
-    public WildPokemonRepository() {
-        wildPokemon = new ArrayList<Pokemon>();
-    }
-
-    public synchronized List<Pokemon> getAll() {
-        return new ArrayList<Pokemon>(wildPokemon);
-    }
-
-    public synchronized boolean insert(Pokemon pokemon) {
-        if(wildPokemon.contains(pokemon)) {
-            return false;
-        }
-
-        long expiry = pokemon.getExpiryMillis();
-
-        int currentIndex = 0;
-        for(Pokemon entry : wildPokemon) {
-
-            if(entry.getExpiryMillis() > expiry) {
-                wildPokemon.add(currentIndex, pokemon);
-                return true;
-            }
-            currentIndex++;
-        }
-        wildPokemon.add(pokemon);
-
-        return true;
-    }
-
-    public synchronized void remove(Pokemon pokemon) {
-        wildPokemon.remove(pokemon);
-    }
-
-    public synchronized List<Pokemon> removeExpiredPokemon() {
-        List<Pokemon> toBeRemoved = new ArrayList<Pokemon>();
-
-        for(Pokemon entry : wildPokemon) {
-            if(entry.getExpiryMillis() < System.currentTimeMillis()) {
-                toBeRemoved.add(entry);
-            }
-        }
-
-        wildPokemon.removeAll(toBeRemoved);
-
-        return toBeRemoved;
-    }
+    List<Pokemon> findByExpired(boolean expired);
 
 }
