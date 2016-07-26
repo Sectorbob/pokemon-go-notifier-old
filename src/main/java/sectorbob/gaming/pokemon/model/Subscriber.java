a3e7edae128d60fddf960fd2e9324c33c8fc71c3
Kyle Heide <heidew@gmail.com>
2016-07-26 15:42:57 -0400

    moved subscriber config to the database
    added a confi for pause time between pogo moves

---
+++ src/main/java/sectorbob/gaming/pokemon/model/Subscriber.java
@@ -0,0 +1,85 @@
+package sectorbob.gaming.pokemon.model;
+
+import sectorbob.gaming.pokemon.config.AppConfig;
+
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.List;
+
+/**
+ * Created by ltm688 on 7/26/16.
+ */
+@Entity
+public class Subscriber {
+
+    @Id
+    String username;
+    String name;
+    String contact;
+    String carrier;
+    @ElementCollection
+    List<String> locationsOfInterest;
+    @ElementCollection
+    List<String> pokemonOfInterest;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public String getCarrier() {
+        return carrier;
+    }
+
+    public void setCarrier(String carrier) {
+        this.carrier = carrier;
+    }
+
+    public List<String> getLocationsOfInterest() {
+        return locationsOfInterest;
+    }
+
+    public void setLocationsOfInterest(List<String> locationsOfInterest) {
+        this.locationsOfInterest = locationsOfInterest;
+    }
+
+    public List<String> getPokemonOfInterest() {
+        return pokemonOfInterest;
+    }
+
+    public void setPokemonOfInterest(List<String> pokemonOfInterest) {
+        this.pokemonOfInterest = pokemonOfInterest;
+    }
+
+    public boolean interestedIn(Pokemon pokemon) {
+        return interestedInArea(pokemon.getGeneralLocation()) && interestedInPokemon(pokemon.getName());
+    }
+
+    private boolean interestedInArea(String area) {
+        return this.getLocationsOfInterest().contains(area) || this.getLocationsOfInterest().contains("all");
+    }
+
+    private boolean interestedInPokemon(String name) {
+        return this.getPokemonOfInterest().contains(name) || this.getPokemonOfInterest().contains("all");
+    }
+}
