11fe069bd1e3c769e7839d7b1c7d57ecaa3dc475
Sectorbob <heidew@gmail.com>
2016-07-26 12:42:41 -0400

    Added query-ability to the pokemon rest api
    /pokemon - optional query parameters: expired=true|false, area=name_of_general_location
    /pokemon/:name - optional query parameters: expired=true|false

--- src/main/java/sectorbob/gaming/pokemon/rest/MyWildPokemonController.java
+++ src/main/java/sectorbob/gaming/pokemon/rest/MyWildPokemonController.java
@@ -1,9 +1,7 @@
 package sectorbob.gaming.pokemon.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import sectorbob.gaming.pokemon.model.Pokemon;
 import sectorbob.gaming.pokemon.repo.WildPokemonRepository;
 
@@ -12,16 +10,34 @@ import java.util.List;
 /**
  * Created by ltm688 on 7/24/16.
  */
-//@RestController
+@RestController
 public class MyWildPokemonController {
 
-    //@Autowired
+    @Autowired
     WildPokemonRepository wildPokemonRepository;
 
     @RequestMapping(value="/pokemon", method = RequestMethod.GET)
-    public List<Pokemon> getPokemon() {
-        //return wildPokemonRepository.getAll();
-        return null;
+    public List<Pokemon> getPokemon(@RequestParam(value = "expired", required = false) Boolean expired,
+                                    @RequestParam(value = "area", required = false) String area) {
+        if(expired == null && area != null) {
+            return wildPokemonRepository.findByGeneralLocation(area);
+        } else if(expired != null && area == null) {
+            return wildPokemonRepository.findByExpired(expired);
+        } else if(expired != null && area != null) {
+            return wildPokemonRepository.findByExpiredAndGeneralLocation(expired, area);
+        } else {
+            return wildPokemonRepository.findAll();
+        }
+    }
+
+    @RequestMapping(value="/pokemon/{name}")
+    public List<Pokemon> getSpecificPokemon(@PathVariable(value = "name") String name,
+                                            @RequestParam(value = "expired", required = false) Boolean expired) {
+        if(expired == null) {
+            return wildPokemonRepository.findByName(name);
+        } else {
+            return wildPokemonRepository.findByExpiredAndName(expired, name);
+        }
     }
 
 }
