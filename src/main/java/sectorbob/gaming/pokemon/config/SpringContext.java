819da73ac91f0a8b9ecd1b08969b410e9378d53d
ltm688 <kyle.heide@capitalone.com>
2016-07-26 08:56:17 -0400

    Added a client to send smd via email
    changed the sms sending from twilio to gmail

--- src/main/java/sectorbob/gaming/pokemon/config/SpringContext.java
+++ src/main/java/sectorbob/gaming/pokemon/config/SpringContext.java
@@ -2,7 +2,9 @@ package sectorbob.gaming.pokemon.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
+import sectorbob.gaming.pokemon.sms.EmailClient;
 
+import javax.mail.MessagingException;
 import java.io.FileNotFoundException;
 
 /**
@@ -12,13 +14,25 @@ import java.io.FileNotFoundException;
 public class SpringContext {
 
     AppConfig appConfig;
+    EmailClient emailClient;
 
     @Bean
     public AppConfig appConfig(ConfigLoader configLoader) throws FileNotFoundException {
         if(appConfig == null) {
-            return configLoader.load();
+            appConfig = configLoader.load();
+            return appConfig;
         } else {
             return appConfig;
+        }
+    }
+
+    @Bean
+    public EmailClient emailClient(AppConfig appConfig) throws MessagingException {
+        if(emailClient == null) {
+            emailClient = new EmailClient(appConfig.getEmail().getUser(), appConfig.getEmail().getPassword());
+            return emailClient;
+        } else {
+            return emailClient;
         }
     }
 
