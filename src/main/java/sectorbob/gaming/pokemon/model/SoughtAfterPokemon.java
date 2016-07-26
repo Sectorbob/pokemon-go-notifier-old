c0c64526b734200634d3bdaeb9cca74cb5e4aa05
ltm688 <kyle.heide@capitalone.com>
2016-07-26 11:37:18 -0400

    Added a database to store the pokemon and notifications sent

--- src/main/java/sectorbob/gaming/pokemon/model/SoughtAfterPokemon.java
+++ src/main/java/sectorbob/gaming/pokemon/model/SoughtAfterPokemon.java
@@ -1,14 +1,41 @@
 package sectorbob.gaming.pokemon.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.OneToOne;
+
 /**
  * Created by ltm688 on 7/25/16.
  */
+@Entity
 public class SoughtAfterPokemon {
 
+    @Id
+    Long encounterId;
+
+    @OneToOne
+    @JoinTable(name="USER_GROUP")
     Pokemon pokemon;
+
     boolean notificationSent;
     String receipt;
 
+    public SoughtAfterPokemon() { }
+
+    public SoughtAfterPokemon(Pokemon pokemon) {
+        setPokemon(pokemon);
+        setEncounterId(pokemon.getEncounterId());
+        setNotificationSent(false);
+    }
+
+    public Long getEncounterId() {
+        return encounterId;
+    }
+
+    public void setEncounterId(Long encounterId) {
+        this.encounterId = encounterId;
+    }
 
     public Pokemon getPokemon() {
         return pokemon;
